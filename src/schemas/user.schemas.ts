ac3e91434f078a0ec1dab388f68eed84e2b18d28
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-24 16:55:34 -0300

    identation

--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-    id: z.number().positive(),
-    name: z.string().min(1),
-    email: z.string().min(1).email(),
-    
-})
+   id: z.number().positive(),
+   name: z.string().min(1),
+   email: z.string().min(1).email(),
+});
