0e51afa40dc48be745873e0069db5d1ff319639e
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-24 17:48:09 -0300

    finishing base

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { json } from "express";
 import helmet from "helmet";
+import "express-async-errors";
+import { HandleErrors } from "./middlewares/handleErrors.middleware";
 
 export const app = express();
 
 app.use(helmet());
 
 app.use(json());
+
+app.use(HandleErrors.execute);
--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -4,4 +4,5 @@ export const userSchema = z.object({
    id: z.number().positive(),
    name: z.string().min(1),
    email: z.string().min(1).email(),
+   password: z.string().min(8)
 });
