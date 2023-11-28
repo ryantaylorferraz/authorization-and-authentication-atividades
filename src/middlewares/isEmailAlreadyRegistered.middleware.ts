6b02735ae130240ea3d1f232eb71640c115af7e5
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-11-28 09:22:25 -0300

    feat: unique user

---
+++ src/middlewares/isEmailAlreadyRegistered.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { prisma } from "../database/prisma";
+import { AppError } from "../errors/appError";
+
+export class IsEmailAlreadyRegistered{
+    static async execute(req: Request, res: Response, next: NextFunction){
+        const user = await prisma.user.findFirst({ where: { email: req.body.email }});
+
+        if(user){
+            throw new AppError("Email already registered", 403);
+        }
+
+        next();
+    }
+}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,12 +3,13 @@ import { container } from "tsyringe";
 import { UserServices } from "../services/user.services";
 import { UserControllers } from "../controllers/user.controllers";
 import { VerifyToken } from "../middlewares/verifyToken.middleware";
+import { IsEmailAlreadyRegistered } from "../middlewares/isEmailAlreadyRegistered.middleware";
 
 container.registerSingleton("UserServices", UserServices);
 const userControllers = container.resolve(UserControllers);
 
 export const userRouter = Router();
 
-userRouter.post("/", (req, res) => userControllers.register(req, res));
+userRouter.post("/", IsEmailAlreadyRegistered.execute, (req, res) => userControllers.register(req, res));
 userRouter.post("/login", (req, res) => userControllers.login(req, res));
 userRouter.get("/", VerifyToken.execute, (req, res) => userControllers.getUser(req, res));
